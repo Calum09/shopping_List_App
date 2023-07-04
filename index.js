352b662a5797052b5de96443e3da70b4350246d6
Calum Laverick <calumlaverick09@gmail.com>
2023-07-04 16:16:42 +0100

    adding the entire project

---
+++ index.js
@@ -0,0 +1,86 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js'
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js"
+
+// Connecting the project firebase database
+const appSettings = {
+    databaseURL: "https://shoppingcart-45173-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+// Configuring the firebase databse
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+
+const inputField = document.getElementById("input-field")
+const addButton = document.getElementById("add-button")
+const shoppingList = document.getElementById("shopping-list")
+
+
+// On button click call this function
+addButton.addEventListener("click", function() { // Look to refactor this function into a separate one and call it on the eventlistener
+    let inputValue = inputField.value
+
+    push(shoppingListInDB, inputValue) // Add the input value to the database
+
+    clearInputField() 
+
+})
+
+// Using the firebase function 'onValue' to grab the snapshot of an item added to the database and append that item to the list in the browser
+onValue(shoppingListInDB, function(snapshot) {
+        
+    if (snapshot.exists()) { // If there's a snapshot run this code
+
+        let itemsArray = Object.entries(snapshot.val()) // convert list of objects to an array 
+
+    
+        clearShoppingList() 
+        
+        // Looping through each item in the database
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+    
+            let currentItemID = currentItem[0] // The key of the item
+            let currentItemValue = currentItem[1] // The value of the item 
+                    
+            // Add the current item to the list
+            addItemToShoppingList(currentItem) 
+        
+    }
+   
+    } else {
+
+        shoppingList.innerHTML = "No items here... yet" // Display this message when there's no items in the shopping list
+    }
+})
+ 
+ // Clear items from the list
+function clearShoppingList() {
+    shoppingList.innerHTML = ""
+}
+
+// Clear the input value after pressing the button 
+function clearInputField() {
+    inputField.value = ""
+}
+
+// Add items from the input into the list
+function addItemToShoppingList(item) {
+
+    let itemID = item[0] // Item key
+    let itemValue = item[1] // Item value
+    
+    let newListElement = document.createElement("li") // Create a new list element
+    
+    newListElement.textContent = itemValue // Add the database value to the list element
+
+    // The callback function will find the unique ID of the item clicked twice and then remove it
+    newListElement.addEventListener("dblclick", function() {
+        let locationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+        
+        remove(locationOfItemInDB)
+    })
+    
+    shoppingList.append(newListElement) // add item to list 
+}
